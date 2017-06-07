f42a5c4c8bc12813fe65a7113a8cce3973027eb4
Dyas <dyas@live.com>
2017-06-07 16:03:38 +0700

    edit view

--- resources/views/resizeImage.blade.php
+++ resources/views/resizeImage.blade.php
@@ -4,8 +4,20 @@
 		<br>
 		<form action="/resizeImagePost" method="post">
 			{{ csrf_field() }}
-			Title : <input type="text" name="title"><br>
-			<input type="file" name="image"><br>
+			Title : <input type="text" name="title">
+			@if ($errors->has('nama'))
+			    <span class="help-block">
+			        <strong>This field is required</strong>
+			    </span>
+			@endif
+			<br>
+			<input type="file" name="image">
+			@if ($errors->has('image'))
+			    <span class="help-block">
+			        <strong>This field is required</strong>
+			    </span>
+			@endif
+			<br>
 			<button type="submit" class="btn btn-success">Upload Image</button>
 		</form>
 	</body>
