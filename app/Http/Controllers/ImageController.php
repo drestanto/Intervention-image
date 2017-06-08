af7ec88fb867ae062af7fa5db57e383afd4eb160
Dyas <dyas@live.com>
2017-06-08 10:51:33 +0700

    resize Image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -41,27 +41,14 @@ class ImageController extends Controller
      */
     public function resizeImage(Request $request)
     {
-	    $this->validate($request, [
-	    	'title' => 'required',
-            'image' => 'required',
-        ]);
-        return "A";
-        $image = $request->file('image');
-        $input['imagename'] = time().'.'.$image->getClientOriginalExtension();
-     
-   
-        $destinationPath = public_path('/thumbnail');
-        $img = Image::make($image->getRealPath());
-        $img->resize(100, 100, function ($constraint) {
-		    $constraint->aspectRatio();
-		})->save($destinationPath.'/'.$input['imagename']);
+	    //Asumsi berhasil
+        $width = (int) $request->width;
+        $height = (int) $request->height;
 
-        return "A";
-        $destinationPath = public_path('/images');
-        $image->move($destinationPath, $input['imagename']);
+        if (($width <= 0) or ($height <= 0)) {
+            Storage::delete($request->path);
+            return "Wrong Input for Width and Height";
+        } return "YEAY";
 
-        $this->postImage->add($input);
-
-        return "Done, resize complete";
     }
 }
--- resources/views/resizeImage.blade.php
+++ resources/views/resizeImage.blade.php
@@ -4,22 +4,11 @@
 		<h3> Resizing images </h3>
 		<form action="/resizeImagePost" method="post">
 			{{ csrf_field() }}
-			Title : <input type="text" name="title" values="{{ $title }}" disabled> <br>
-			Path : <input type="text" name="path" values="{{ $path }}" disabled> <br>
-			Width : <input type="text" name="width">
-			@if ($errors->has('width'))
-			    <span class="help-block">
-			        <strong>This field is required</strong>
-			    </span>
-			@endif
-			<br>
+			Title : <input type="text" name="title" value="{{ $title }}" disabled> <br>
+			Path : <input type="text" name="path" value="{{ $path }}" disabled> <br><br>
+			<strong> Resize to </strong><br>
+			Width : <input type="text" name="width"><br>
 			Height : <input type="text" name="height"><br>
-			@if ($errors->has('height'))
-			    <span class="help-block">
-			        <strong>This field is required</strong>
-			    </span>
-			@endif
-			<br>
 			<button type="submit" class="btn btn-success">Resize</button>
 		</form>
 	</body>
